2cc28b55f4518fa5f670c776d7cba19acf5645ef
woojeongKong <wjkong@elecvery.com>
2023-10-06 16:32:41 +0900

    feature: 샘플 자료 수정

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
+import { Link } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
       <div className="flex justify-center gap-[100px] m-[200px] ">
-        <div
+        <Link
+          to="/stroke"
           className="w-[300px] h-[200px] flex flex-col justify-center items-center bg-[#D6CC99] rounded-lg cursor-pointer  font-mono antialiased font-bold"
-          onClick={() => {
-            window.location.href = "/stroke"
-          }}
         >
           SAMPLE 1
-        </div>
-        <div
+        </Link>
+        <Link
+          to="/card"
           className="w-[300px] h-[200px] flex justify-center items-center bg-[#FDE5D4] rounded-lg cursor-pointer  font-mono antialiased font-bold"
-          onClick={() => {
-            window.location.href = "/card"
-          }}
         >
           SAMPLE 2
-        </div>
+        </Link>
       </div>
     </div>
   );
