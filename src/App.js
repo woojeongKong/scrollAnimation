dc566047a9c8821d5f493fa031861046aa0b74eb
woojeongKong <wjkong@elecvery.com>
2023-10-06 15:59:49 +0900

    feature: 샘플 자료 라우팅

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import { CardAnimation } from "./pages/cardAnimation/CardAnimation";
-import { StrokeAnimation } from "./pages/strokeAnimation/StrokeAnimation";
-
 function App() {
   return (
     <div className="App">
-      <StrokeAnimation />
-      {/*<CardAnimation />*/}
+      <div className="grid grid-rows-1 grid-flow-col gap-4 m-[200px]">
+        <div
+          className="w-[300px] h-[200px] flex flex-col justify-center items-center bg-[#D6CC99] rounded-lg cursor-pointer  font-mono antialiased font-bold"
+          onClick={() => {
+            window.location.href = "/stroke"
+          }}
+        >
+          SAMPLE 1
+        </div>
+        <div
+          className="w-[300px] h-[200px] flex justify-center items-center bg-[#FDE5D4] rounded-lg cursor-pointer  font-mono antialiased font-bold"
+          onClick={() => {
+            window.location.href = "/card"
+          }}
+        >
+          SAMPLE 2
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,10 +4,35 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { StrokeAnimation } from "./pages/strokeAnimation/StrokeAnimation";
+import { CardAnimation } from "./pages/cardAnimation/CardAnimation";
+
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/stroke",
+    element: <StrokeAnimation />,
+  },
+  {
+    path: "/card",
+    element: <CardAnimation />
+  }
+]);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/*<App />*/}
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
